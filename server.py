bc72a5169c652cf504b763aed745feed2b345af3
carobinson <carly.a.robinson@gmail.com>
2015-08-18 00:46:00 -0700

    added some styling and bootstrap

--- server.py
+++ server.py
@@ -7,10 +7,12 @@ from flask_login import LoginManager
 from jinja2 import StrictUndefined
 from flask import Flask, render_template, redirect, request, flash, session, jsonify
 from flask_debugtoolbar import DebugToolbarExtension
+from flask_bootstrap import Bootstrap
 from model import Play, Scene, Genre, Character, Monologue, User, Comment, connect_to_db, db
 
 
 app = Flask(__name__)
+Bootstrap(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 
