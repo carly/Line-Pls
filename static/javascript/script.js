ae83230e937299b4904474ae27acffe1e5435481
carobinson <carly.a.robinson@gmail.com>
2015-08-22 13:48:09 -0700

    fixing homepage front-end

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -1,71 +1,96 @@
 
-//Constants for the SVG
-var width = 500,
-    height = 500;
+var width = 960,
+    height = 500,
+    root;
 
-//Set up the colour scale
-var color = d3.scale.category20();
 
-//Set up the force layout
-var force = d3.layout.force()
-    .charge(-120)
-    .linkDistance(30)
-    .size([width, height]);
-
-//Append a SVG to the body of the html page. Assign this SVG as an object to svg
 var svg = d3.select("body").append("svg")
     .attr("width", width)
     .attr("height", height);
 
-//Read the data from the mis element
-var mis = document.getElementById('mis').innerHTML;
-graph = JSON.parse(mis);
 
-//Creates the graph data structure out of the json data
-force.nodes(graph.nodes)
-    .links(graph.links)
-    .start();
-
-//Create all the line svgs but without locations yet
-var link = svg.selectAll(".link")
-    .data(graph.links)
-    .enter().append("line")
-    .attr("class", "link")
-    .style("stroke-width", function (d) {
-    return Math.sqrt(d.value);
+d3.json("/shakespeare.json", function(error, json) {
+if (error) return console.warn(error);
+root = json;
+visualizeit(json);
 });
 
-//Do the same with the circles for the nodes - no
-var node = svg.selectAll(".node")
-    .data(graph.nodes)
-    .enter().append("circle")
-    .attr("class", "node")
-    .attr("r", 8)
-    .style("fill", function (d) {
-    return color(d.group);
-})
-    .call(force.drag);
+function visualizeit(data) {
+  var nodes = data.nodes;
+  var links = data.links;
+
+  // Restart the force layout.
+  debugger
+  var force = d3.layout.force()
+      .nodes(d3.values(nodes))
+      .links(links)
+      .size([width, height])
+      .on("tick", tick)
+      .charge(-200)
+      .start();
+
+  var link = svg.selectAll(".link")
+                .data(force.links())
+                .enter().append("line")
+                .attr("class", "link");
+
+  var node = svg.selectAll(".node")
+                .data(force.nodes())
+                .enter().append("g")
+                .attr("class", "node")
+                .call(force.drag);
 
 
-//Now we are giving the SVGs co-ordinates - the force layout is generating the co-ordinates which this code is using to update the attributes of the SVG elements
-force.on("tick", function () {
-    link.attr("x1", function (d) {
-        return d.source.x;
-    })
-        .attr("y1", function (d) {
-        return d.source.y;
-    })
-        .attr("x2", function (d) {
-        return d.target.x;
-    })
-        .attr("y2", function (d) {
-        return d.target.y;
-    });
+  var color = d3.scale.category20();
 
-    node.attr("cx", function (d) {
-        return d.x;
-    })
-        .attr("cy", function (d) {
-        return d.y;
-    });
-});
+
+  // Enter any new links.
+  link
+      .attr("x1", function(d) { return d.source.x; })
+      .attr("y1", function(d) { return d.source.y; })
+      .attr("x2", function(d) { return d.target.x; })
+      .attr("y2", function(d) { return d.target.y; });
+
+
+  // Exit any old nodes.
+
+
+  // Enter any new nodes.
+  node.append("circle")
+      .attr("class", "node")
+      .attr("cx", function(d) { return d.x; })
+      .attr("cy", function(d) { return d.y; })
+      .attr("r", function(d) { return Math.sqrt(d.size) / 10 || 4.5; })
+      .style("fill", color)
+      .on("click", click)
+      .call(force.drag);
+}
+
+function tick() {
+  link.attr("x1", function(d) { return d.source.x; })
+      .attr("y1", function(d) { return d.source.y; })
+      .attr("x2", function(d) { return d.target.x; })
+      .attr("y2", function(d) { return d.target.y; });
+
+  node.attr("cx", function(d) { return d.x; })
+      .attr("cy", function(d) { return d.y; });
+}
+
+// Color leaf nodes orange, and packages white or blue.
+function color(d) {
+  return d._children ? "#3182bd" : d.children ? "#c6dbef" : "#fd8d3c";
+}
+
+// Toggle children on click.
+function click(d) {
+  if (!d3.event.defaultPrevented) {
+    if (d.children) {
+      d._children = d.children;
+      d.children = null;
+    } else {
+      d.children = d._children;
+      d._children = null;
+    }
+    // visualizeit();
+  }
+}
